68aa5ce96fabb206f3c8e8d5bf22e31f4b1119cb
flintman <flintman@flintmancomputers.com>
2015-09-14 08:27:41 -0400

    tenderloin-common:  adjust Art settings per
    https://source.android.com/devices/tech/dalvik/configure.html
    
    Change-Id: I4ca43e0365bf2f31b64d1b147009b12a86e9e866

--- tenderloin-common.mk
+++ tenderloin-common.mk
@@ -168,4 +168,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.networklocation=1 \
     dalvik.vm.lockprof.threshold=500 \
     dalvik.vm.dexopt-flags=m=y \
-    dalvik.vm.dex2oat-flags=--no-watch-dog
+    dalvik.vm.dex2oat-flags=--no-watch-dog \
+    dalvik.vm.dex2oat-filter=interpret-only \
+    dalvik.vm.image-dex2oat-filter=speed
