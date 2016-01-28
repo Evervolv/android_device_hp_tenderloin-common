968b2848baa32b3b174475dc4243ad0132622fc0
Arne Coucheron <arco68@gmail.com>
2016-01-29 00:18:10 +0100

    Sync media codecs with LA.AF.1.2.1_rb1.5
    Change-Id: Id71f4bc783461236938bbf59562eceed2fbca5fa

--- tenderloin-common.mk
+++ tenderloin-common.mk
@@ -136,8 +136,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    device/hp/tenderloin-common/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     device/hp/tenderloin-common/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    device/hp/tenderloin-common/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    device/hp/tenderloin-common/configs/media_codecs_google_performance.xml:system/etc/media_codecs_google_performance.xml \
+    device/hp/tenderloin-common/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+
+PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
