bd2b8205946392e93f2a9f25a12c810d4d5a6110
William Bellavance <flintman@flintmancomputers.com>
2016-08-30 20:40:41 -0400

    wifi:  Update to switch back to kernel builtin
    Change-Id: I512074b7a1b78f5390b3332de3663d450bcf6f6e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -148,6 +148,3 @@ BOARD_WLAN_DEVICE                := ath6kl
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_ath6kl
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/ath6kl.ko"
-WIFI_DRIVER_MODULE_NAME          := "ath6kl"
-WIFI_DRIVER_LOADER_DELAY         := 1000000
