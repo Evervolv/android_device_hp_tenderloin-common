d1ebbac8b277368b048d6cbbc1d7fb5eed2a31ff
Yifan Hong <elsk@google.com>
2017-03-28 00:28:24 +0000

    Enable graphics allocator binderized service on msm8660
    Bug: 36598525
    Bug: 36462585
    
    Test: boots (100/100 times), takes 24080.66ms to boot on avg
    Test: lshal shows graphics allocator
    
    This reverts commit 60f6ec2d638291e9d2e5ed85b06605ec15974cb1.
    
    Change-Id: I1ef9e4aa2a497bc999b9b935603f089f7828e1e9

--- tenderloin-common.mk
+++ tenderloin-common.mk
@@ -59,6 +59,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl
 
 # Filesystem management tools
