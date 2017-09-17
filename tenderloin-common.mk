80c5d0574c4e927d2430d75dffd7738ba92d87b0
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    tenderloin: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff

--- tenderloin-common.mk
+++ tenderloin-common.mk
@@ -249,6 +249,10 @@ PRODUCT_PACKAGES += \
     mkimage \
     lvm.static
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
