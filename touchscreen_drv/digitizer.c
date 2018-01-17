57cbf35acf3ea758dac834dce1cdff0d88bc2cd6
Nick Reuter <nreuter85@gmail.com>
2018-01-16 22:54:30 -0600

    touchscreen: cleanup code
     change tabs to spaces
     remove lingering code
    
    Change-Id: I44774f6f24b1c20014583f1f792cff50e357dd5c

--- touchscreen_drv/digitizer.c
+++ touchscreen_drv/digitizer.c
@@ -45,39 +45,39 @@ void touchscreen_power(int enable)
     ALOGI("touchscreen_power: enable=%d, ts_state=%d", enable, ts_state);
 
     if (enable && !ts_state) {
-		int retry_count = 0;
+        int retry_count = 0;
 try_again:
-		/* Set reset so the chip immediatelly sees it */
+        /* Set reset so the chip immediatelly sees it */
         lseek(xres_fd, 0, SEEK_SET);
         rc = write(xres_fd, "1", 1);
-		if (rc != 1)
-			ALOGE("TSpower, failed set xres");
+        if (rc != 1)
+            ALOGE("TSpower, failed set xres");
 
-		/* Then power on */
+        /* Then power on */
         lseek(vdd_fd, 0, SEEK_SET);
         rc = write(vdd_fd, "1", 1);
-		if (rc != 1)
-			ALOGE("TSpower, failed to enable vdd");
+        if (rc != 1)
+            ALOGE("TSpower, failed to enable vdd");
 
-		/* Sleep some more for the voltage to stabilize */
+        /* Sleep some more for the voltage to stabilize */
         usleep(50000);
 
         lseek(wake_fd, 0, SEEK_SET);
         rc = write(wake_fd, "1", 1);
-		if (rc != 1)
-			ALOGE("TSpower, failed to assert wake");
+        if (rc != 1)
+            ALOGE("TSpower, failed to assert wake");
 
         lseek(xres_fd, 0, SEEK_SET);
         rc = write(xres_fd, "0", 1);
-		if (rc != 1)
-			ALOGE("TSpower, failed to reset xres");
+        if (rc != 1)
+            ALOGE("TSpower, failed to reset xres");
 
         usleep(50000);
 
         lseek(wake_fd, 0, SEEK_SET);
         rc = write(wake_fd, "0", 1);
-		if (rc != 1)
-			ALOGE("TSpower, failed to deassert wake");
+        if (rc != 1)
+            ALOGE("TSpower, failed to deassert wake");
 
         usleep(50000);
 
@@ -91,90 +91,90 @@ try_again:
         i2c_msg.len = 2;
         i2c_buf[0] = 0x08; i2c_buf[1] = 0;
         rc = ioctl(i2c_fd,I2C_RDWR,&i2c_ioctl_data);
-		if (rc != 1)
-			ALOGE("TSPower, ioctl1 failed %d errno %d\n", rc, errno);
-		/* Ok, so the TS failed to wake, we need to retry a few times
-		 * before totally giving up */
-		if ((rc != 1) && (retry_count++ < MAX_DIGITIZER_RETRY)) {
-			lseek(vdd_fd, 0, SEEK_SET);
-			rc = write(vdd_fd, "0", 1);
-			usleep(10000);
-			ALOGE("TS wakeup retry #%d\n", retry_count);
-			goto try_again;
-		}
+        if (rc != 1)
+            ALOGE("TSPower, ioctl1 failed %d errno %d\n", rc, errno);
+        /* Ok, so the TS failed to wake, we need to retry a few times
+         * before totally giving up */
+        if ((rc != 1) && (retry_count++ < MAX_DIGITIZER_RETRY)) {
+            lseek(vdd_fd, 0, SEEK_SET);
+            rc = write(vdd_fd, "0", 1);
+            usleep(10000);
+            ALOGE("TS wakeup retry #%d\n", retry_count);
+            goto try_again;
+        }
 
         i2c_msg.len = 6;
         i2c_buf[0] = 0x31; i2c_buf[1] = 0x01; i2c_buf[2] = 0x08;
         i2c_buf[3] = 0x0C; i2c_buf[4] = 0x0D; i2c_buf[5] = 0x0A;
         rc = ioctl(i2c_fd,I2C_RDWR,&i2c_ioctl_data);
-		if (rc != 1)
-			ALOGE("TSPower, ioctl2 failed %d errno %d\n", rc, errno);
+        if (rc != 1)
+            ALOGE("TSPower, ioctl2 failed %d errno %d\n", rc, errno);
 
         i2c_msg.len = 2;
         i2c_buf[0] = 0x30; i2c_buf[1] = 0x0F;
         rc = ioctl(i2c_fd,I2C_RDWR,&i2c_ioctl_data);
-		if (rc != 1)
-			ALOGE("TSPower, ioctl3 failed %d errno %d\n", rc, errno);
+        if (rc != 1)
+            ALOGE("TSPower, ioctl3 failed %d errno %d\n", rc, errno);
 
         i2c_buf[0] = 0x40; i2c_buf[1] = 0x02;
         rc = ioctl(i2c_fd,I2C_RDWR,&i2c_ioctl_data);
-		if (rc != 1)
-			ALOGE("TSPower, ioctl4 failed %d errno %d\n", rc, errno);
+        if (rc != 1)
+            ALOGE("TSPower, ioctl4 failed %d errno %d\n", rc, errno);
 
         i2c_buf[0] = 0x41; i2c_buf[1] = 0x10;
         rc = ioctl(i2c_fd,I2C_RDWR,&i2c_ioctl_data);
-		if (rc != 1)
-			ALOGE("TSPower, ioctl5 failed %d errno %d\n", rc, errno);
+        if (rc != 1)
+            ALOGE("TSPower, ioctl5 failed %d errno %d\n", rc, errno);
 
         i2c_buf[0] = 0x0A; i2c_buf[1] = 0x04;
         rc = ioctl(i2c_fd,I2C_RDWR,&i2c_ioctl_data);
-		if (rc != 1)
-			ALOGE("TSPower, ioctl6 failed %d errno %d\n", rc, errno);
+        if (rc != 1)
+            ALOGE("TSPower, ioctl6 failed %d errno %d\n", rc, errno);
 
         i2c_buf[0] = 0x08; i2c_buf[1] = 0x03;
         rc = ioctl(i2c_fd,I2C_RDWR,&i2c_ioctl_data);
-		if (rc != 1)
-			ALOGE("TSPower, ioctl7 failed %d errno %d\n", rc, errno);
+        if (rc != 1)
+            ALOGE("TSPower, ioctl7 failed %d errno %d\n", rc, errno);
 
         lseek(wake_fd, 0, SEEK_SET);
         rc = write(wake_fd, "1", 1);
-		if (rc != 1)
-			ALOGE("TSpower, failed to assert wake again");
-		ts_state = 1;
+        if (rc != 1)
+            ALOGE("TSpower, failed to assert wake again");
+        ts_state = 1;
     } else if (ts_state) {
         lseek(vdd_fd, 0, SEEK_SET);
         rc = write(vdd_fd, "0", 1);
-		if (rc != 1)
-			ALOGE("TSpower, failed to disable vdd");
+        if (rc != 1)
+            ALOGE("TSpower, failed to disable vdd");
 
-		/* Weird, but on 4G touchpads even after vdd is off there is still
-		 * stream of data from ctp that only disappears after we reset the
-		 * touchscreen, even though it's supposedly powered off already
-		 */
+        /* Weird, but on 4G touchpads even after vdd is off there is still
+         * stream of data from ctp that only disappears after we reset the
+         * touchscreen, even though it's supposedly powered off already
+         */
         lseek(xres_fd, 0, SEEK_SET);
         rc = write(xres_fd, "1", 1);
-		usleep(10000);
+        usleep(10000);
         lseek(xres_fd, 0, SEEK_SET);
         rc = write(xres_fd, "0", 1);
         /* XXX, should be correllated with LIFTOFF_TIMEOUT in ts driver */
         usleep(80000);
-		ts_state = 0;
+        ts_state = 0;
     }
 }
 
 void init_digitizer_fd(void) {
-	/* TS file descriptors. Ignore errors. */
-	vdd_fd = open("/sys/devices/platform/cy8ctma395/vdd", O_WRONLY);
-	if (vdd_fd < 0)
-		ALOGE("TScontrol: Cannot open vdd - %d", errno);
-	xres_fd = open("/sys/devices/platform/cy8ctma395/xres", O_WRONLY);
-	if (xres_fd < 0)
-		ALOGE("TScontrol: Cannot open xres - %d", errno);
-	wake_fd = open("/sys/user_hw/pins/ctp/wake/level", O_WRONLY);
-	if (wake_fd < 0)
-		ALOGE("TScontrol: Cannot open wake - %d", errno);
-	i2c_fd = open("/dev/i2c-5", O_RDWR);
-	if (i2c_fd < 0)
-		ALOGE("TScontrol: Cannot open i2c dev - %d", errno);
+    /* TS file descriptors. Ignore errors. */
+    vdd_fd = open("/sys/devices/platform/cy8ctma395/vdd", O_WRONLY);
+    if (vdd_fd < 0)
+        ALOGE("TScontrol: Cannot open vdd - %d", errno);
+    xres_fd = open("/sys/devices/platform/cy8ctma395/xres", O_WRONLY);
+    if (xres_fd < 0)
+        ALOGE("TScontrol: Cannot open xres - %d", errno);
+    wake_fd = open("/sys/user_hw/pins/ctp/wake/level", O_WRONLY);
+    if (wake_fd < 0)
+        ALOGE("TScontrol: Cannot open wake - %d", errno);
+    i2c_fd = open("/dev/i2c-5", O_RDWR);
+    if (i2c_fd < 0)
+        ALOGE("TScontrol: Cannot open i2c dev - %d", errno);
 
 }
--- touchscreen_drv/ts_srv_set.c
+++ touchscreen_drv/ts_srv_set.c
@@ -45,91 +45,91 @@
 #define SOCKET_BUFFER_SIZE 1
 
 int receive_ts_mode(int ts_fd) {
-	// Receives the mode from touchscreen socket
-	struct timeval seltmout;
-	fd_set fdset;
-	int sel_ret, recv_ret;
-	char recv_str[SOCKET_BUFFER_SIZE];
+    // Receives the mode from touchscreen socket
+    struct timeval seltmout;
+    fd_set fdset;
+    int sel_ret, recv_ret;
+    char recv_str[SOCKET_BUFFER_SIZE];
 
-	seltmout.tv_sec = 0;
-	seltmout.tv_usec = TS_SOCKET_TIMEOUT;
-	FD_ZERO(&fdset);
-	FD_SET(ts_fd, &fdset);
-	sel_ret = select(ts_fd + 1, &fdset, NULL, NULL, &seltmout);
-	if (sel_ret == 0) {
-		ALOGE("Unable to retrieve current mode - timeout\n");
-		return -40;
-	} else {
-		recv_ret = recv(ts_fd, recv_str, SOCKET_BUFFER_SIZE, 0);
-		if (recv_ret > 0) {
-			if ((int)recv_str[0] == 0)
-				printf("Finger mode\n");
-			else if ((int)recv_str[0] == 1)
-				printf("Stylus mode\n");
-			else {
-				ALOGI("Unknown mode '%i'\n", (int)recv_str[0]);
-				return -60;
-			}
-			return 0;
-		} else {
-			ALOGE("Error receiving mode\n");
-			return -50;
-		}
-	}
+    seltmout.tv_sec = 0;
+    seltmout.tv_usec = TS_SOCKET_TIMEOUT;
+    FD_ZERO(&fdset);
+    FD_SET(ts_fd, &fdset);
+    sel_ret = select(ts_fd + 1, &fdset, NULL, NULL, &seltmout);
+    if (sel_ret == 0) {
+        ALOGE("Unable to retrieve current mode - timeout\n");
+        return -40;
+    } else {
+        recv_ret = recv(ts_fd, recv_str, SOCKET_BUFFER_SIZE, 0);
+        if (recv_ret > 0) {
+            if ((int)recv_str[0] == 0)
+                printf("Finger mode\n");
+            else if ((int)recv_str[0] == 1)
+                printf("Stylus mode\n");
+            else {
+                ALOGI("Unknown mode '%i'\n", (int)recv_str[0]);
+                return -60;
+            }
+            return 0;
+        } else {
+            ALOGE("Error receiving mode\n");
+            return -50;
+        }
+    }
 }
 
 int send_ts_socket(char *send_data) {
-	// Connects to the touchscreen socket
-	struct sockaddr_un unaddr;
-	int ts_fd, len;
+    // Connects to the touchscreen socket
+    struct sockaddr_un unaddr;
+    int ts_fd, len;
 
-	ts_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    ts_fd = socket(AF_UNIX, SOCK_STREAM, 0);
 
-	if (ts_fd >= 0) {
-		unaddr.sun_family = AF_UNIX;
-		strcpy(unaddr.sun_path, TS_SOCKET_LOCATION);
-		len = strlen(unaddr.sun_path) + sizeof(unaddr.sun_family);
-		if (connect(ts_fd, (struct sockaddr *)&unaddr, len) >= 0) {
-			int send_ret;
-			send_ret = send(ts_fd, send_data, sizeof(*send_data), 0);
-			if (send_ret <= 0) {
-				ALOGE("Unable to send data to socket\n");
-				return -30;
-			} else {
-				if ((strcmp(send_data, "F") == 0)) {
-					ALOGI("Touchscreen set for finger mode\n");
-					return 0;
-				} else if ((strcmp(send_data, "S") == 0)) {
-					ALOGI("Touchscreen set for stylus mode\n");
-					return 0;
-				} else {
-					// Get the current mode
-					return receive_ts_mode(ts_fd);
-				}
-			}
-		} else {
-			ALOGE("Unable to connect socket\n");
-			return -20;
-		}
-		close(ts_fd);
-	} else {
-		ALOGE("Unable to create socket\n");
-		return -10;
-	}
+    if (ts_fd >= 0) {
+        unaddr.sun_family = AF_UNIX;
+        strcpy(unaddr.sun_path, TS_SOCKET_LOCATION);
+        len = strlen(unaddr.sun_path) + sizeof(unaddr.sun_family);
+        if (connect(ts_fd, (struct sockaddr *)&unaddr, len) >= 0) {
+            int send_ret;
+            send_ret = send(ts_fd, send_data, sizeof(*send_data), 0);
+            if (send_ret <= 0) {
+                ALOGE("Unable to send data to socket\n");
+                return -30;
+            } else {
+                if ((strcmp(send_data, "F") == 0)) {
+                    ALOGI("Touchscreen set for finger mode\n");
+                    return 0;
+                } else if ((strcmp(send_data, "S") == 0)) {
+                    ALOGI("Touchscreen set for stylus mode\n");
+                    return 0;
+                } else {
+                    // Get the current mode
+                    return receive_ts_mode(ts_fd);
+                }
+            }
+        } else {
+            ALOGE("Unable to connect socket\n");
+            return -20;
+        }
+        close(ts_fd);
+    } else {
+        ALOGE("Unable to create socket\n");
+        return -10;
+    }
 }
 
 int main(int argc, char** argv)
 {
-	if (argc != 2 || strlen(argv[1]) != 1 ||
-		(strcmp(argv[1], "F") != 0 && strcmp(argv[1], "S") != 0 &&
-		strcmp(argv[1], "M") != 0)) {
-		printf("Please supply exactly 1 argument:\n");
-		printf("F to set finger mode\n");
-		printf("S to set stylus mode\n");
-		printf("M to display the current setting\n");
-		printf("This is used to set the mode of operation for the\n");
-		printf("touchscreen driver on the TouchPad\n");
-		return -1;
-	} else
-		return send_ts_socket(argv[1]);
+    if (argc != 2 || strlen(argv[1]) != 1 ||
+        (strcmp(argv[1], "F") != 0 && strcmp(argv[1], "S") != 0 &&
+        strcmp(argv[1], "M") != 0)) {
+        printf("Please supply exactly 1 argument:\n");
+        printf("F to set finger mode\n");
+        printf("S to set stylus mode\n");
+        printf("M to display the current setting\n");
+        printf("This is used to set the mode of operation for the\n");
+        printf("touchscreen driver on the TouchPad\n");
+        return -1;
+    } else
+        return send_ts_socket(argv[1]);
 }
