f4130c9c615b699ad3bc7bfff4f19be077b30b10
shumash <shumashgeely@gmail.com>
2018-05-08 09:31:30 -0600

    tenderloin:  add seccomp_policy This also fixes Chrome issues
    Patch sets 2 & 3:  Fix up folder and file naming
    Change-Id: I8c1ec6a5bf6be65211c750e37fdaf5f9aee27dea

--- tenderloin-common.mk
+++ tenderloin-common.mk
@@ -205,6 +205,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
 # Stlport
 #PRODUCT_PACKAGES += \
 #   libstlport
