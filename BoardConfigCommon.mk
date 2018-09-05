5fffb3c95795ba41e73eec19870ed3ccfa01c44e
Nick Reuter <nreuter85@gmail.com>
2018-09-04 23:26:02 -0500

    tenderloin: change TARGET_CPU_VARIANT to scorpion
    This reverts commit aaeb1b4d6c55ad5ea0f59dd84c68151dd8547c72.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -16,7 +16,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := cortex-a8
+TARGET_CPU_VARIANT := scorpion
 TARGET_HAVE_TSLIB := false
 
 # Art
